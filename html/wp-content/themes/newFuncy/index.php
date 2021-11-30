c5836863a4fa4f8177d2f8a528613450bb2c61a2
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-11-30 10:50:59 +0100

    Conejo Capoeria.

--- html/wp-content/themes/newFuncy/index.php
+++ html/wp-content/themes/newFuncy/index.php
@@ -10,8 +10,15 @@
     <div id="div_principal">
         <p id="contenedor_imagen"><img src="https://i.gifer.com/8CUe.gif" id="imagen"></p>
         <br>
-        <h3 id="h3_titulo">Conejo Full Stack Developer</h3>
+        <h3 id="h3_titulo">Desenvolvedor Rabbit Full Stack</h3>
         <p id="p_texto">Este coelho é o coelho Full Stack Developer, pode ser apreciado pelo incrível cansaço que sente quando está na frente do computador. Ele trabalha incansavelmente para alimentar sua família. Simplesmente um conchasumadre.</p>
+    </div>
+
+    <div id="div_principal2">
+        <p id="contenedor_imagen"><img src="https://www.lavanguardia.com/files/content_image_mobile_filter/uploads/2021/03/26/605daf04cfa16.jpeg" id="imagen"></p>
+        <br>
+        <h3 id="h3_titulo">Pinho Coelho</h3>
+        <p id="p_texto">Este coelho conchesumadre que anda sobre duas pernas, simplesmente épico, coelho da capoeira.</p>
     </div>
 
     <? get_header() ?>
