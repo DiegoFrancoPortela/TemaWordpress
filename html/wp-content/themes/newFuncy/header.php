f6793a68456d04f06ef37a93914bb0637a55107a
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-11-30 10:35:42 +0100

    Conejo Full Stack

--- html/wp-content/themes/newFuncy/header.php
+++ html/wp-content/themes/newFuncy/header.php
@@ -1,5 +1,4 @@
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 
 <div id="cabecera">
-    <h1>Conejo</h1>
 </div>
--- html/wp-content/themes/newFuncy/index.php
+++ html/wp-content/themes/newFuncy/index.php
@@ -5,17 +5,16 @@
         <title>New Funcy</title>
     <body>
 
-    <?php get_header() ?>
+    <div id="bg-image"></div>
 
-    <div id="cuerpo">
-
-        <?php
-            echo ("<h1> Conejo Full Stack Developer </h1>")
-        ?>
-        <img src="https://i.gifer.com/8CUe.gif">
+    <div id="div_principal">
+        <p id="contenedor_imagen"><img src="https://i.gifer.com/8CUe.gif" id="imagen"></p>
+        <br>
+        <h3 id="h3_titulo">Conejo Full Stack Developer</h3>
+        <p id="p_texto">Este coelho é o coelho Full Stack Developer, pode ser apreciado pelo incrível cansaço que sente quando está na frente do computador. Ele trabalha incansavelmente para alimentar sua família. Simplesmente um conchasumadre.</p>
     </div>
 
-    <? get_footer() ?>
+    <? get_header() ?>
 
     </body>
 </html>
